b81b2bfdcb11d10afadbad1533f6079445ce58cc
kevinospina142 <kh5564765@gmail.com>
2023-02-24 17:56:37 -0500

    24/feb

--- scripts/main.js
+++ scripts/main.js
@@ -12,4 +12,23 @@ imagen.addEventListener('click', ()=>{
         container.classList.add('bordeOculto');
     }
 })
-//
+// num mayor de 10
+
+const num1 = document.getElementById('numgato');
+const num2 = document.getElementById('numgato2');
+const num3 = document.getElementById('numshrek');
+const suma = num1+num2+num3;
+const text = document.getElementById('textnum');
+const btnVerificar = document.getElementById('btnVerificar');
+
+btnVerificar.addEventListener('click', ()=>{
+    if (suma > 10){
+        text.innerHTML = "Hola buenas tardes";
+    }else{
+        text.innerHTM = "Hola buenas noches";
+        return text;
+    }
+});
+
+
+
