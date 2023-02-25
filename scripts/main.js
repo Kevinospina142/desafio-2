cd7aab4f59c1a6e01129b12c8681e79de89c76a0
kevinospina142 <kh5564765@gmail.com>
2023-02-24 19:13:43 -0500

    24/feb

--- scripts/main.js
+++ scripts/main.js
@@ -11,24 +11,40 @@ imagen.addEventListener('click', ()=>{
         container.classList.remove('bordeVisible');
         container.classList.add('bordeOculto');
     }
-})
+});
 // num mayor de 10
 
-const num1 = document.getElementById('numgato');
-const num2 = document.getElementById('numgato2');
-const num3 = document.getElementById('numshrek');
-const suma = num1+num2+num3;
-const text = document.getElementById('textnum');
+
 const btnVerificar = document.getElementById('btnVerificar');
 
 btnVerificar.addEventListener('click', ()=>{
-    if (suma > 10){
-        text.innerHTML = "Hola buenas tardes";
+    const num1 = parseInt(document.getElementById('numgato').value);
+    const num2 = parseInt(document.getElementById('numgato2').value);
+    const num3 = parseInt(document.getElementById('numshrek').value);
+    const suma = (num1 + num2 + num3);
+    const text = document.getElementById('textnum');
+    if (suma <= 10){
+        text.innerHTML = "llevas "+suma+" stickers";
     }else{
-        text.innerHTM = "Hola buenas noches";
-        return text;
+
+        text.innerHTML = "LLevas demasiados stickers";
     }
 });
 
+// contraseña
+const btnIngresar = document.getElementById("btnIngresar");
 
+btnIngresar.addEventListener('click', ()=>{
+    const pass1 = parseInt(document.getElementById('pass1').value);
+    const pass2 = parseInt(document.getElementById('pass2').value);
+    const pass3 = parseInt(document.getElementById('pass3').value);
+    const text2 = document.getElementById('textPass');
+     if (pass1 === 9 && pass2 === 1 && pass3 === 1){
+        text2.innerHTML = "password 1 correcta";
+     }else if(pass1 === 7 && pass2 === 1 && pass3 === 4){
+        text2.innerHTML = "password 2 correcta";
+     }else{
+        text2.innerHTML = "password incorrecto";
+     }
+});
 
